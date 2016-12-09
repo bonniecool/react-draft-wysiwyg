aea6cf9d558ca8024e5f5c6fcb21883e0fdf11a9
bonniecool <bonniecool.3@gmail.com>
2016-12-09 14:18:40 +0800

    Update index.js
    warning: <div> cannot be descendant of <p>
    <p>
     <div>text</div>
    </p>

--- js/src/components/Editor/index.js
+++ js/src/components/Editor/index.js
@@ -207,7 +207,7 @@ export default class WysiwygEditor extends Component {
   customBlockRenderMap: Map = DefaultDraftBlockRenderMap
   .merge(new Map({
     unstyled: {
-      element: 'p',
+      element: 'div',
     },
   }));
 
